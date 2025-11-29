f840f07a4bc85c558afda10aff4613c92458dceb
Lucas Eduardo <lept.eng21@uea.edu.br>
2025-11-29 18:05:48 -0400

    feat crud usuario

--- polls/urls.py
+++ polls/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import criar_curriculo, criar_vaga
+from .views import criar_curriculo, criar_vaga, criar_usuario, obter_usuario, avaliar_curriculo, editar_usuario
 
 from . import views
 
@@ -8,4 +8,8 @@ urlpatterns = [
     path('curriculos/criar/', views.criar_curriculo, name='criar_curriculo'),
     path('vagas/criar/', views.criar_vaga, name='criar_vaga'),
     path('curriculos/avaliar/<int:curriculum_id>/<int:job_id>/', views.avaliar_curriculo, name='avaliar_curriculo'),
+    path('usuario/criar/', views.criar_usuario, name='criar_usuario'),
+    path('usuario/editar/<int:user_id>/', views.editar_usuario, name='editar_usuario'),
+    path('usuario/deletar/<int:user_id>/', views.deletar_usuario, name='deletar_usuario'),
+    path('usuario/<int:user_id>/', views.obter_usuario, name='obter_usuario'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import JsonResponse
-from .models import Curriculum, Job
+from .models import Curriculum, Job, User
 from django.views.decorators.csrf import csrf_exempt
 import json
 from src.pipeline.evaluator import analyze_resume
@@ -40,7 +40,57 @@ def criar_vaga(request):
         )
 
         return JsonResponse({"message": "Vaga criada com sucesso!", "id": vaga.id})
+
+@csrf_exempt
+def criar_usuario(request):
+    if request.method == "POST":
+        data = json.loads(request.body)
+
+        usuario = User.objects.create(
+            idUsuario=data.get("idUsuario"),
+            name=data.get("name"),
+            adress=data.get("adress"),
+            phone=data.get("phone"),
+            age=data.get("age")
+        )
+
+        return JsonResponse({"message": "Usuário criado com sucesso!"})
     
+def obter_usuario(request, user_id):
+    """Pega o usuário do banco de dados pelo ID."""
+    usuario = User.objects.get(id=user_id)
+    usuario_data = {
+        "idUsuario": usuario.idUsuario,
+        "name": usuario.name,
+        "adress": usuario.adress,
+        "phone": usuario.phone,
+        "age": usuario.age,
+    }
+    return JsonResponse(usuario_data)
+
+@csrf_exempt
+def editar_usuario(request, user_id):
+    """Edita o usuário no banco de dados pelo ID."""
+    if request.method == "PUT":
+        data = json.loads(request.body)
+        usuario = User.objects.get(id=user_id)
+
+        usuario.idUsuario = data.get("idUsuario", usuario.idUsuario)
+        usuario.name = data.get("name", usuario.name)
+        usuario.adress = data.get("adress", usuario.adress)
+        usuario.phone = data.get("phone", usuario.phone)
+        usuario.age = data.get("age", usuario.age)
+        usuario.save()
+
+        return JsonResponse({"message": "Usuário atualizado com sucesso!"})
+    
+@csrf_exempt
+def deletar_usuario(request, user_id):
+    """Deleta o usuário do banco de dados pelo ID."""
+    if request.method == "DELETE":
+        usuario = User.objects.get(id=user_id)
+        usuario.delete()
+        return JsonResponse({"message": "Usuário deletado com sucesso!"})
 
 def avaliar_curriculo(request, curriculum_id, job_id):
     """Pega o currículo e a vaga do banco e envia pro modelo fazer o score."""
